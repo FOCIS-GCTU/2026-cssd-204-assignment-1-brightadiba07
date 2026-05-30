6652ee68fc5451e80f533ef9cab8e47a3ce3f4cc
brightadiba07 <147569601+brightadiba07@users.noreply.github.com>
2026-05-30 16:00:47 +0000

    refactor: update initials.py with detailed header and formatted output

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
-
+    # File: initials.py
+    # Description: Print out my initials
+    # Assignment Number : 1
+    # Name: Bright Adiba
+    # Student ID : 2425402044
+    # Email: brightadiba07@gmail.com
+    # Grader: Augustus Buckman
+    # On my honor, BRIGHT ADIBA, this programming assignment is my own work
+    # and I have not provided this code to any other student.
 def main():
-  pass #code goes here
+    #Print out my initials in Large styled size
+     print()
+     print("...BAB")
+     print()
 
+     print("...BBBBBBBBBBBB........AAAAAAAAAAAA........BBBBBBBBBBBB.....")
+     print("...BB........BB........AA........AA........BB........BB.....")
+     print("...BB........BB........AA........AA........BB........BB.....")
+     print("...BB........BB........AA........AA........BB........BB.....")
+     print("...BBBBBBBBBBBB........AAAAAAAAAAAA........BBBBBBBBBBBB.....")
+     print("...BB........BB........AA........AA........BB........BB.....")
+     print("...BB........BB........AA........AA........BB........BB.....")
+     print("...BB........BB........AA........AA........BB........BB.....")
+     print("...BB........BB...**...AA........AA...**...BB........BB...**")
+     print("...BBBBBBBBBBBB...**...AA........AA...**...BBBBBBBBBBBB...**")
 
+     print()
 
 main()
